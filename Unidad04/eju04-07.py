7918ce9b193e4a95fb2d8b7e17609d68ab6ab4f5
horavazquez <92801664+horavazquez@users.noreply.github.com>
2024-08-19 15:49:47 -0300

    Update Repositorio

---
+++ Unidad04/eju04-07.py
@@ -0,0 +1,13 @@
+def square(sample):
+    """Función que calcula los cuadrados de una lista de números.
+    Parámetros
+    sample: Es una lista de números
+    Devuelve una lista con los cuadrados de los números de la lista sample.
+    """
+    list = []
+    for i in sample:
+        list.append(i**2)
+    return list
+
+print(square([1, 2, 3, 4, 5]))
+print(square([2.3, 5.7, 6.8, 9.7, 12.1, 15.6]))
