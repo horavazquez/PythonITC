8a2d8272e85080f4968f3fc138d4409794b1bafe
horavazquez <92801664+horavazquez@users.noreply.github.com>
2024-08-19 15:54:14 -0300

    Update Repositorio

---
+++ Unidad04/calculos.py
@@ -0,0 +1,24 @@
+from operaciones import *
+
+a, b, c, d = (10, 5, 0, "Hola")
+
+print("\nSUMA:")
+try:
+    print( "{} + {} = {}".format(a, d, suma(a, d) ) )
+except TypeError as e:
+    print("El tipo de dato ingresado no es valido \n###",e, "###")
+
+print("\nRESTA:")
+try:
+    print( "{} - {} = {}".format(b, a, resta(b, a) ) )
+except TypeError as e:    
+    print("El tipo de dato ingresado no es valido \n###",e, "###")
+
+print("\nPRODUCTO:")
+try:
+    print( "{} * {} = {}".format(b, d, producto(b, d) ) )
+except TypeError as e:    
+    print("El tipo de dato ingresado no es valido \n###",e, "###")
+
+
+#print( "{} / {} = {}".format(a, c, division(a, c) ) )
