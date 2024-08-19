56fd6cdc72dae243d388e822cb9954fd7cc99dc1
horavazquez <92801664+horavazquez@users.noreply.github.com>
2024-08-19 15:42:14 -0300

    Update Repositorio

---
+++ Unidad04/eju04-03.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    """Función que calcula el factorial de un número entero positivo.
+    Parámetros
+    n: Es un entero positivo.
+    Devuelve el factorial de n.
+    """
+    tmp = 1
+    for i in range(n):
+        tmp *= i+1
+    return tmp
+
+print(factorial(4))
+print(factorial(20))
